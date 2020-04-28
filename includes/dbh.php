aad64c7d567f28c1cda51fbc13eeae514314bac2
brixzen <brixzen@hotmail.com>
2020-04-27 21:19:40 -0400

    First commit

---
+++ includes/dbh.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root"; //Root default for xampp Changed if online server
+$dbPassword = ""; //Default for xampp
+$dbName = "Specter";
+
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword, $dbName);
+ ?>
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+  include_once 'includes/dbh.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+  </head>
+  <body>
+
+    $conn;
+
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="videos.html">Your Vides</a></li>
+      <li><a href="news.asp">News</a></li>
+      <li><a href="contact.asp">Contact</a></li>
+      <li><a href="about.asp">About</a></li>
+    </ul>
+
+
+
+ <h1 class="text-light">Specter</h1>
+<!--
+ <div class="grid-container">
+   <div class="grid-item">1</div>
+   <div class="grid-item">2</div>
+   <div class="grid-item">3</div>
+   <div class="grid-item">4</div>
+   <div class="grid-item">5</div>
+   <div class="grid-item">6</div>
+   <div class="grid-item">7</div>
+   <div class="grid-item">8</div>
+   <div class="grid-item">9</div>
+ </div> -->
+  </body>
+
+</html>
